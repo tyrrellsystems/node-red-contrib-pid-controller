6d309423bc7767366d9bb75ac375d82b611238d3
Ben Hardill <hardillb@gmail.com>
2016-11-24 10:04:53 +0000

    Run on own loop and flip Fire flag

--- pid.js
+++ pid.js
@@ -23,6 +23,7 @@ module.exports = function(RED) {
 		this.Kp = n.Kp;
 		this.Ki = n.Ki;
 		this.Kd = n.Kd;
+		this.recalcTime = n.recalcTime;
 		this.setPointTopic = n.setPointTopic;
 		this.fireTopic = n.fireTopic;
 		this.fixedTopic = n.fixedTopic;
@@ -62,7 +63,7 @@ module.exports = function(RED) {
 				node.setPoint = msg.payload;
 				node.status({text: 'setpoint ' + node.setPoint});
 			} else if (msg.topic && msg.topic === node.fireTopic) {
-				if (msg.payload) {
+				if (!msg.payload) {
 					node.status({text: 'FIRE', shape: 'dot', fill: 'red'});
 					var newMsg = {
 						topic: node.topic,
@@ -103,15 +104,102 @@ module.exports = function(RED) {
 					node.status({text: 'Fixed ' + node.fixedValue, fill:'green',shape:'dot'});
 				}
 			} else {
+				node.measured = msg.payload;
 				//console.log("value");
-				if (node.lastTimestamp) {
-				    var now = Date.now();
-				    var dt = (now - node.lastTimestamp)/1000;
+				// if (node.lastTimestamp) {
+				//     var now = Date.now();
+				//     var dt = (now - node.lastTimestamp)/1000;
+				//     //console.log("dt: ", dt)
+				//     node.lastTimestamp = now;
+				// 	var measured = msg.payload;
+				// 	//console.log("measured %d", measured);
+				// 	var errorVal = node.setPoint - measured;
+				// 	// if (Math.abs(errorVal) <= node.deadBand) {
+				// 	// 	var newMsg = {
+				// 	// 		topic: node.topic,
+				// 	// 		payload: 0,
+				// 	// 	};
+				// 	// 	if (!node.fire && !node.fixed) {
+				// 	// 		node.send([newMsg,newMsg]);
+				// 	// 		node.errorVal = errorVal;
+				// 	// 		node.status({fill:"green",shape:"dot", text: 'setpoint ' + node.setPoint});
+				// 	// 	}
+
+				// 	// 	node.integral = 0
+
+				// 	// 	return;
+				// 	// }
+				// 	//console.log("errorVal %d", errorVal);
+				// 	var integral = node.integral + (errorVal * dt);
+
+				// 	//console.log("integral %d", integral);
+				// 	//TODO TESTING
+				// 	if (Math.abs(integral) > node.maxOutput) {
+				// 		if (integral > 0) {
+				// 			integral = node.maxOutput;
+				// 		} else {
+				// 			integral = node.maxOutput * -1;
+				// 		}
+				// 	} 
+				// 	//console.log("integral (after max) %d", integral);
+
+				// 	node.integral = integral;
+				// 	var derivitive = (errorVal - node.errorVal)/dt;
+				// 	//console.log("derivitive %d", derivitive);
+				// 	node.errorVal = errorVal;
+				// 	var output = (node.Kp*errorVal) + (node.Ki*integral) + (node.Kd*derivitive);
+				// 	if (Math.abs(output) > node.maxOutput) {
+				// 		if (output > 0) {
+				// 			output = node.maxOutput;
+				// 		} else {
+				// 			output = node.maxOutput * -1;
+				// 		}
+				// 	}
+
+				// 	//console.log("output %d", output);
+				// 	var newMsg = {
+				// 		topic: node.topic,
+				// 		payload: output,
+				// 	};
+				// 	var newMsg2 = {
+				// 		topic: node.topic,
+				// 		payload: 0,
+				// 	};
+
+				// 	var array = [];
+				// 	if (output > 0) {
+				// 		array = [newMsg, newMsg2];
+				// 	} else {
+				// 		newMsg.payload = newMsg.payload * -1;
+				// 		array = [newMsg2, newMsg];
+				// 	}
+
+				// 	if (!node.fire && !node.fixed) { 
+				// 		node.send(array);
+				// 	}
+				// 	var status = {fill:"green",shape:"ring", text: 'setpoint ' + node.setPoint}; 
+				// 	if (output > 0) {
+				// 		status.fill = "red";
+				// 	} else {
+				// 		status.fill = "blue";
+				// 	}
+				// 	node.status(status);
+				// 	//console.log("%j",node);
+				// } else {
+				// 	node.lastTimestamp = Date.now();
+				// }
+			}
+		});
+
+		this.interval = setInterval(function(){
+			if (node.measured) {
+					var now = Date.now();
+				    var dt = node.recalcTime;//(now - node.lastTimestamp)/1000;
 				    //console.log("dt: ", dt)
 				    node.lastTimestamp = now;
-					var measured = msg.payload;
+
 					//console.log("measured %d", measured);
-					var errorVal = node.setPoint - measured;
+					var errorVal = node.setPoint - node.measured;
 					// if (Math.abs(errorVal) <= node.deadBand) {
 					// 	var newMsg = {
 					// 		topic: node.topic,
@@ -183,15 +271,15 @@ module.exports = function(RED) {
 					}
 					node.status(status);
 					//console.log("%j",node);
-				} else {
-					node.lastTimestamp = Date.now();
-				}
 			}
-		});
+		},node.recalcTime * 1000);
 
 		this.on('close', function(){
 			if (node.fireResetInterval) {
 				clearTimeout(node.fireResetInterval);
+			}
+			if (node.interval) {
+				clearInterval(node.interval);
 			}
 		});
 
