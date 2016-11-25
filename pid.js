26e5c59b19916702e598f7e63828b9d2bd539cfc
Ben Hardill <hardillb@gmail.com>
2016-11-25 09:35:29 +0000

    Rework of pid function

--- pid.js
+++ pid.js
@@ -20,10 +20,10 @@ module.exports = function(RED) {
 	function PIDLoop(n) {
 		RED.nodes.createNode(this, n);
 		this.topic = n.topic;
-		this.Kp = n.Kp;
-		this.Ki = n.Ki;
-		this.Kd = n.Kd;
-		this.recalcTime = n.recalcTime;
+		this.P = Number(n.Kp);
+		this.Ti = Number(n.Ki);
+		this.Td = Number(n.Kd);
+		this.dt = Number(n.recalcTime);
 		this.setPointTopic = n.setPointTopic;
 		this.fireTopic = n.fireTopic;
 		this.fixedTopic = n.fixedTopic;
@@ -32,7 +32,7 @@ module.exports = function(RED) {
 		this.deadBand = n.deadBand;
 
 		this.minOutput = 0;
-		this.maxOutput = 100;
+		this.maxOutput = 1;
 
 		this.errorVal = 0;
 		this.integral = 0;
@@ -54,7 +54,12 @@ module.exports = function(RED) {
 			node.lastTimestamp = 0;
 			node.errorVal = 0;
 			node.integral = 0;
-			delete node.measured;
+			if (node.measured) {
+				delete node.measured;
+			}
+			if (node.lastMeasured) {
+				delete node.lastMeasured;
+			}
 		}
 
 		this.on('input', function(msg){
@@ -105,175 +110,141 @@ module.exports = function(RED) {
 					node.status({text: 'Fixed ' + node.fixedValue, fill:'green',shape:'dot'});
 				}
 			} else {
-				node.measured = msg.payload;
-				//console.log("value");
-				// if (node.lastTimestamp) {
-				//     var now = Date.now();
-				//     var dt = (now - node.lastTimestamp)/1000;
-				//     //console.log("dt: ", dt)
-				//     node.lastTimestamp = now;
-				// 	var measured = msg.payload;
-				// 	//console.log("measured %d", measured);
-				// 	var errorVal = node.setPoint - measured;
-				// 	// if (Math.abs(errorVal) <= node.deadBand) {
-				// 	// 	var newMsg = {
-				// 	// 		topic: node.topic,
-				// 	// 		payload: 0,
-				// 	// 	};
-				// 	// 	if (!node.fire && !node.fixed) {
-				// 	// 		node.send([newMsg,newMsg]);
-				// 	// 		node.errorVal = errorVal;
-				// 	// 		node.status({fill:"green",shape:"dot", text: 'setpoint ' + node.setPoint});
-				// 	// 	}
-
-				// 	// 	node.integral = 0
-
-				// 	// 	return;
-				// 	// }
-				// 	//console.log("errorVal %d", errorVal);
-				// 	var integral = node.integral + (errorVal * dt);
-
-				// 	//console.log("integral %d", integral);
-				// 	//TODO TESTING
-				// 	if (Math.abs(integral) > node.maxOutput) {
-				// 		if (integral > 0) {
-				// 			integral = node.maxOutput;
-				// 		} else {
-				// 			integral = node.maxOutput * -1;
-				// 		}
-				// 	} 
-				// 	//console.log("integral (after max) %d", integral);
-
-				// 	node.integral = integral;
-				// 	var derivitive = (errorVal - node.errorVal)/dt;
-				// 	//console.log("derivitive %d", derivitive);
-				// 	node.errorVal = errorVal;
-				// 	var output = (node.Kp*errorVal) + (node.Ki*integral) + (node.Kd*derivitive);
-				// 	if (Math.abs(output) > node.maxOutput) {
-				// 		if (output > 0) {
-				// 			output = node.maxOutput;
-				// 		} else {
-				// 			output = node.maxOutput * -1;
-				// 		}
-				// 	}
-
-				// 	//console.log("output %d", output);
-				// 	var newMsg = {
-				// 		topic: node.topic,
-				// 		payload: output,
-				// 	};
-				// 	var newMsg2 = {
-				// 		topic: node.topic,
-				// 		payload: 0,
-				// 	};
-
-				// 	var array = [];
-				// 	if (output > 0) {
-				// 		array = [newMsg, newMsg2];
-				// 	} else {
-				// 		newMsg.payload = newMsg.payload * -1;
-				// 		array = [newMsg2, newMsg];
-				// 	}
-
-				// 	if (!node.fire && !node.fixed) { 
-				// 		node.send(array);
-				// 	}
-				// 	var status = {fill:"green",shape:"ring", text: 'setpoint ' + node.setPoint}; 
-				// 	if (output > 0) {
-				// 		status.fill = "red";
-				// 	} else {
-				// 		status.fill = "blue";
-				// 	}
-				// 	node.status(status);
-				// 	//console.log("%j",node);
-				// } else {
-				// 	node.lastTimestamp = Date.now();
-				// }
+				if (typeof msg.payload === 'number') {
+					node.measured = msg.payload;
+					node.lastMeasured = node.measured;
+				}
 			}
 		});
 
+
+
 		this.interval = setInterval(function(){
-			if (node.measured) {
-					var now = Date.now();
-				    var dt = node.recalcTime;//(now - node.lastTimestamp)/1000;
-				    //console.log("dt: ", dt)
-				    node.lastTimestamp = now;
+			if (node.measured && node.lastMeasured) {
 
-					//console.log("measured %d", measured);
-					var errorVal = node.setPoint - node.measured;
-					// if (Math.abs(errorVal) <= node.deadBand) {
-					// 	var newMsg = {
-					// 		topic: node.topic,
-					// 		payload: 0,
-					// 	};
-					// 	if (!node.fire && !node.fixed) {
-					// 		node.send([newMsg,newMsg]);
-					// 		node.errorVal = errorVal;
-					// 		node.status({fill:"green",shape:"dot", text: 'setpoint ' + node.setPoint});
-					// 	}
+				var error = node.setPoint - node.measured;
 
-					// 	node.integral = 0
+				// if (Math.abs(error) < node.deadBand) {
+				// 	console.log("in deadband");
+				// 	error = 0;
+				// }
 
-					// 	return;
-					// }
-					//console.log("errorVal %d", errorVal);
-					var integral = node.integral + (errorVal * dt);
+				var deltaError = node.measured - node.lastMeasured;
 
-					//console.log("integral %d", integral);
-					//TODO TESTING
-					if (Math.abs(integral) > node.maxOutput) {
-						if (integral > 0) {
-							integral = node.maxOutput;
-						} else {
-							integral = node.maxOutput * -1;
-						}
-					} 
-					//console.log("integral (after max) %d", integral);
+				console.log("error: " + error);
 
-					node.integral = integral;
-					var derivitive = (errorVal - node.errorVal)/dt;
-					//console.log("derivitive %d", derivitive);
-					node.errorVal = errorVal;
-					var output = (node.Kp*errorVal) + (node.Ki*integral) + (node.Kd*derivitive);
-					if (Math.abs(output) > node.maxOutput) {
-						if (output > 0) {
-							output = node.maxOutput;
-						} else {
-							output = node.maxOutput * -1;
-						}
-					}
+				var integral = (error * node.dt * node.P) / (node.Ti * 100);
+				console.log("integral: " + integral);
 
-					//console.log("output %d", output);
-					var newMsg = {
-						topic: node.topic,
-						payload: output,
-					};
-					var newMsg2 = {
-						topic: node.topic,
-						payload: 0,
-					};
+				//var output = (1/node.P) * (error + (node.Td * deltaError)/node.dt) + ((node.integral * node.dt) / node.Ti);
+				var output = (error * node.P/100) + node.integral;
 
-					var array = [];
+				var diff = (node.Td *deltaError)/node.dt;
+				console.log("diff:" + diff);
+
+				
+
+				console.log("power: " +  output);
+				if (Math.abs(output) > node.maxOutput) {
 					if (output > 0) {
-						array = [newMsg, newMsg2];
+						output = node.maxOutput;
 					} else {
-						newMsg.payload = newMsg.payload * -1;
-						array = [newMsg2, newMsg];
+						output = node.maxOutput * -1;
 					}
+				} else {
+					node.integral = node.integral + integral;
+					console.log("node.integral: " + node.integral);
+				}
+				output = Math.round(output * 10000) / 1000;
+				console.log("adjusted: " + output);
+				console.log("---------------");
 
-					if (!node.fire && !node.fixed) { 
-						node.send(array);
-					}
-					var status = {fill:"green",shape:"ring", text: 'setpoint ' + node.setPoint}; 
+				var msg = {
+					topic: node.topic || "",
+					payload: output
+				}
+
+				var off = {
+					topic: node.topic || "",
+					payload: 0
+				}
+
+				if (!node.fixed && !node.fire) {
 					if (output > 0) {
-						status.fill = "red";
+						node.send([msg,off]);
+
 					} else {
-						status.fill = "blue";
+						node.send([off,msg]);
 					}
-					node.status(status);
-					//console.log("%j",node);
+				}
+
+				node.lastMeasured = node.measured;
+
+			 //    var dt = node.recalcTime;
+			 //    //(now - node.lastTimestamp)/1000;
+			 //    //console.log("dt: ", dt)
+			 //    node.lastTimestamp = now;
+				// //console.log("measured %d", node.measured);
+				// var errorVal = node.setPoint - node.measured;
+				// //console.log("error %d", errorVal);
+
+				// var integral = node.integral + (errorVal * dt);
+
+				// console.log("integral %d", integral);
+				// //TODO TESTING
+				// if (Math.abs(integral) > node.maxOutput) {
+				// 	if (integral > 0) {
+				// 		integral = node.maxOutput;
+				// 	} else {
+				// 		integral = node.maxOutput * -1;
+				// 	}
+				// } 
+				// //console.log("integral (after max) %d", integral);
+
+				// node.integral = integral;
+				// var derivitive = (errorVal - node.errorVal)/dt;
+				// //console.log("derivitive %d", derivitive);
+				// node.errorVal = errorVal;
+				// var output = (node.Kp*errorVal) + (node.Ki*integral) + (node.Kd*derivitive);
+				// if (Math.abs(output) > node.maxOutput) {
+				// 	if (output > 0) {
+				// 		output = node.maxOutput;
+				// 	} else {
+				// 		output = node.maxOutput * -1;
+				// 	}
+				// }
+
+				// //console.log("output %d", output);
+				// var newMsg = {
+				// 	topic: node.topic,
+				// 	payload: output,
+				// };
+				// var newMsg2 = {
+				// 	topic: node.topic,
+				// 	payload: 0,
+				// };
+
+				// var array = [];
+				// if (output > 0) {
+				// 	array = [newMsg, newMsg2];
+				// } else {
+				// 	newMsg.payload = newMsg.payload * -1;
+				// 	array = [newMsg2, newMsg];
+				// }
+
+				// if (!node.fire && !node.fixed) { 
+				// 	node.send(array);
+				// }
+				// var status = {fill:"green",shape:"ring", text: 'setpoint ' + node.setPoint}; 
+				// if (output > 0) {
+				// 	status.fill = "red";
+				// } else {
+				// 	status.fill = "blue";
+				// }
+				// node.status(status);
+				//console.log("%j",node);
 			}
-		},node.recalcTime * 1000);
+		},node.dt * 1000);
 
 		this.on('close', function(){
 			if (node.fireResetInterval) {
